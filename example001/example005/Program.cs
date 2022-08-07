e0b5aa6cfb189f38d88bff61c7583b5ef0e57ec6
Lilay <Lilay4618@mail.ru>
2022-08-07 20:50:09 +0500

    лоавлоырао

---
+++ example001/example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юляша")
+{
+    Console.WriteLine("Ура, Юля!!!");
+}
+else
+{
+    Console.Write("Пешёл ты, ");
+    Console.WriteLine(username);
+}
